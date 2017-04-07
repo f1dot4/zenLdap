9cc251200c49c48989f975e1d13a5b85dbce7a17
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2017-04-07 13:44:54 +0200

    make login options more versatile

--- ldapLogon.php
+++ ldapLogon.php
@@ -21,7 +21,6 @@ class ldapLogon {
 		setOptionDefault('ldapServer', '192.168.1.14');
 		setOptionDefault('ldapServerPort','389');
 		setOptionDefault('ldapSearchBase','cn=users,cn=accounts,dc=example,dc=com');
-		setOptionDefault('ldapSearchBaseGroup','cn=groups,cn=accounts,dc=example,dc=com');
 		setOptionDefault('ldapLoginAttribute','cn');
 		setOptionDefault('ldapAuthAttribute','cn');
 		setOptionDefault('ldapUserFilter','');
@@ -32,7 +31,7 @@ class ldapLogon {
 	}
 	function getOptionsSupported(){
 		$options = 	array(
-			gettext('Enable LDAP Logon') => array('key' => 'ldapEnabled', 
+			gettext('Enable LDAP Logon') => array('key' => 'ldapEnabled',
 								'type' => OPTION_TYPE_CHECKBOX,
 								'order' => 0,
 								'desc' => gettext("Set the checkbox to enable the LDAP logon functionality")
@@ -52,46 +51,42 @@ class ldapLogon {
 								'order' => 3,
 								'desc' => gettext("Set the LDAP search base for users"),
 							),
-			gettext('LDAP Search Base for Groups') => array('key' => 'ldapSearchBaseGroup',
-								'type' => OPTION_TYPE_TEXTBOX,
-								'order' => 4,
-								'desc' => gettext("Set the LDAP search base for groups"),
-							),
 			gettext('LDAP Login Attribute') => array('key' => 'ldapLoginAttribute',
 								'type' => OPTION_TYPE_TEXTBOX,
-								'order' => 5,
+								'order' => 4,
 								'desc' => gettext("Set the LDAP attribute the users must enter on ZenPhoto to login (e.g. mail to login with mailaddress, uid to login with username, etc.)"),
 							),
 			gettext('LDAP Auth Attribute') => array('key' => 'ldapAuthAttribute',
 								'type' => OPTION_TYPE_TEXTBOX,
-								'order' => 6,
+								'order' => 5,
 								'desc' => gettext("Set the LDAP attribute used to authenticate against LDAP (e.g. the first part of a fully distinguished name)"),
 							),
 			gettext('LDAP User Filter') => array('key' => 'ldapUserFilter',
 								'type' => OPTION_TYPE_TEXTBOX,
-								'order' => 7,
+								'order' => 6,
 								'desc' => gettext("Set an additional filter for users (e.g.: (memberOf=cn=zenphoto-user,cn=groups,cn=accounts,dc=example,dc=com) to only allow users that are members of the zenphoto-user LDAP group to log in)"),
 							),
 			gettext('LDAP Reader DN') => array('key' => 'ldapReaderDn',
 								'type' => OPTION_TYPE_TEXTBOX,
-								'order' => 8,
+								'order' => 7,
 								'desc' => gettext("Set the DN of an LDAP-user with read permissions for the user and group objects"),
 							),
 			gettext('LDAP Reader Password') => array('key' => 'ldapReaderPass',
 								'type' => OPTION_TYPE_PASSWORD,
-								'order' => 9,
+								'order' => 8,
 								'desc' => gettext("Password of the LDAP-User with read permissions for the user and group objects"),
 							),
 			gettext('Default template') => array('key' => 'ldapZenDefaultTemplate',
 								'type' => OPTION_TYPE_TEXTBOX,
-								'order' => 10,
+								'order' => 9,
 								'desc' => gettext("Template that should be used when no ldap zen-group is available")
 							)
 		);
 		return $options;
 	}
+
 	function handleOption($option, $currentValue) {
-  	}
+	}
 
 	/*
 	 * returns a LDAP connection
@@ -111,29 +106,16 @@ class ldapLogon {
 		}
 	}
 
-	/*
-	 * Returns the array uids of a cn
-	 *
-	 * @param ldapServer
-	 * @param ldapServerPort
-	 * @param ldapDc in format 'dc=domain' or 'dc=domain,dc=tld'
-	 * @param ldapCn in format 'cn=user'
-	 */
-	static function getLdapUidsOfCn($ldapServer,$ldapServerPort,$ldapDc,$ldapCn) {
-		if($ldapServer == NULL || $ldapServerPort == NULL || $ldapDc == NULL || $ldapCn == NULL) {
-			debugLog('LDAP: function getLdapUidsOfCn() called with at least one invalid argument!');
-			return false;
-		}
-		if($ldapC = self::getLdapConnection($ldapServer,$ldapServerPort)) {
-			if(@ldap_bind($ldapC, getOption('ldapRdrDn'), getOption('ldapRdrPass'))) {
-				$res = ldap_search($ldapC,$ldapDc, $ldapCn);
-				$user = ldap_get_entries($ldapC,$res);
-				unset($user[0]['uid']['count']);
+	static function getLdapObjects($ldapServer, $ldapServerPort, $ldapSearchBase, $filter){
+		if ($ldapC = self::getLdapConnection($ldapServer,$ldapServerPort)){
+			if(@ldap_bind($ldapC, getOption('ldapReaderDn'), getOption('ldapReaderPass'))){
+				$res = ldap_search($ldapC, $ldapSearchBase, $filter);
+				$objects = ldap_get_entries($ldapC, $res);
 				ldap_close($ldapC);
-				return $user[0]['uid'];
+				return $objects;
 			} else {
 				debugLog("Cannot bind to LDAP-server:".ldap_error($ldapC));
-                                ldap_close($ldapC);
+				ldap_close($ldapC);
 				return false;
 			}
 		} else {
@@ -141,119 +123,40 @@ class ldapLogon {
 			return false;
 		}
 	}
-	
-	/*
-	 * Returns an array of CNs of a given uid
-	 *
-	 * @param ldapServer
-	 * @param ldapServerPort
-	 * @param ldapDc in format 'dc=domain' or 'dc=domain,dc=tld'
-	 * @param ldapUid in format 'memberUid=user'
-	 * @param ldapAttr
-	 */
-	static function getLdapGroupCnsOfUid($ldapServer,$ldapServerPort,$ldapDc,$ldapUid , $ldapAttr = "") {
-		if($ldapServer == NULL || $ldapServerPort == NULL || $ldapDc == NULL || $ldapUid == NULL || $ldapAttr == NULL) {
-			debugLog('LDAP: function getLdapZenUsers() called with at least one invalid argument!');
-			return false;
-		}
-		if($ldapC = self::getLdapConnection($ldapServer,$ldapServerPort)) {
-			if(@ldap_bind($ldapC, getOption('ldapRdrDn'), getOption('ldapRdrPass'))) {
-				$groups = array();
-				$res = ldap_search($ldapC,$ldapDc, $ldapUid, $ldapAttr);
-				$user = ldap_get_entries($ldapC,$res);
-				for($i=0;$i < count($user); $i++) {
-						if(!empty($user[$i]['cn']['0'])) {
-								$groups[$i] = $user[$i]['cn']['0'];
-						}
-				}
-				ldap_close($ldapC);
-				return $groups;
-			} else {
-				debugLog("Cannot bind to LDAP-server:".ldap_error($ldapC));
-                                ldap_close($ldapC);
-				return false;
-			}
-		} else {
-			debugLog("Cannot connect to LDAP-server:".ldap_error($ldapC));
-			return false;
-		}
-	}	
 
-	/*
-         * Return an Array with the group of a given user
-         * @param ldapServer
-         * @param ldapServerPort
-         * @param ldapDc in format 'dc=domain' or 'dc=domain,dc=tld'
-         * @param ldapCn in format 'cn=user'
-         * @param ldapFilter is the ldap-filter attribute, typically "memberof"
-         */
-	static function getAdUserGroups($ldapServer, $ldapServerPort, $ldapDc, $ldapCn,$ldapFilter = array('memberof')) {
-                if($ldapC = self::getLdapConnection($ldapServer,$ldapServerPort)) {
-                        if(@ldap_bind($ldapC, getOption('ldapRdrDn'), getOption('ldapRdrPass'))) {
-	                        $res = ldap_search($ldapC,$ldapDc, $ldapCn,$ldapFilter) or die(ldap_error($ldapC));
-	                        $groups = ldap_get_entries($ldapC,$res);
-	                        ldap_close($ldapC);
-				if(array_key_exists('memberof',$groups[0]) ){
-		                        $tmp=$groups[0]['memberof'];
-		                        $groups=array();
-	        	                unset($tmp['count']);
-	                	        foreach($tmp as $member) {
-	                        	        $t=explode(",",$member);
-	                                	$t=explode("=",$t[0]);
-		                                array_push($groups,$t[1]);
-		                        }
-	        	                return $groups;
-				} else {
-					debugLog("Groups cannot be retrieved, check your LDAP-Settings, especially the DC, Reader, Readerpassword; Search called with: ldap_search($ldapC,$ldapDc, $ldapCn,$ldapFilter)");
-					
-					return array();
-				}
-			} else { 
-				debugLog("Cannot bind to LDAP-server:".ldap_error($ldapC));
-				ldap_close($ldapC);
-				return false;
-			}
-                } else {
-			debugLog("Cannot connect to LDAP-server:".ldap_error($ldapC));
-                        return false;
-                }
-        }
-	
 	/*
 	 * Return an Array that is used by the external_auth.php script
 	 * @param ldapServer
 	 * @param ldapServerPort
-	 * @param ldapDc in format 'dc=domain' or 'dc=domain,dc=tld'
+	 * @param ldapSearchBase in DN format
 	 * @param user is the string from the login-textbox
 	 * @param defaultZenGroup is the default template that should be used when no group matches
 	 */
-	static function getExternalAuthArray($ldapServer,$ldapServerPort,$ldapDc, $user, $defaultZenGroup) {
-		if($ldapServer == NULL || $ldapServerPort == NULL || $ldapDc == NULL || $user == NULL) {
+	static function getExternalAuthArray($ldapServer, $ldapServerPort, $ldapSearchBase, $user, $defaultZenGroup) {
+		if($ldapServer == NULL || $ldapServerPort == NULL || $ldapSearchBase == NULL || $user == NULL) {
 			debugLog('LDAP: function getExternalAuthArray() called with at least one invalid argument!');
 			return false;
 		}
 		$result=array();
 		$i=0;
 		$result['groups']=array();
-		if(getOption('ldapType') == 'ldapTypeOL' && $ldapUids = self::getLdapUidsOfCn($ldapServer,$ldapServerPort,$ldapDc,'cn='.$user)) { // OpenLDAP is used
-			foreach($ldapUids as $uid) {
-				if(count(self::getLdapGroupCnsOfUid($ldapServer,$ldapServerPort,$ldapDc,'memberUid='.$uid, array('cn')))) {
-					$result['groups'] = array_merge($result['groups'],self::getLdapGroupCnsOfUid($ldapServer,$ldapServerPort,$ldapDc,'memberUid='.$uid, array('cn')));
-				}
+
+		$ldapObject = self::getLdapObjects($ldapServer, $ldapServerPort, $ldapSearchBase,
+			"(&".getOption('ldapUserFilter')."(".getOption('ldapLoginAttribute')."=$user))");
+		if ($ldapObject != false){
+			foreach ($ldapObject[0]['memberof'] as $group){
+				$group = preg_replace("/,.*$/", "", $group);
+				$group = preg_replace("/^.*=/", "", $group);
+				$result['groups'] = array_merge($result['groups'], array("$group"));
 			}
-		} else if (($groups = self::getAdUserGroups($ldapServer,$ldapServerPort,$ldapDc,'cn='.$user)) && getOption('ldapType') == 'ldapTypeAD') {  //MS AD is used
-			$result['groups'] = array_merge($result['groups'],$groups);
 		}
-		if(getOption('ldapType')){ 
-			$result['user'] = $user;
-        	        $result['id'] = '';
-                	$result['defaultgroup'] = $defaultZenGroup;
-			return $result;
-		} else {
-			return NULL;
-		}
+		$result['user'] = $user;
+		$result['id'] = $ldapObject[0][getOption('ldapAuthAttribute')][0];
+		$result['defaultgroup'] = $defaultZenGroup;
+		debugLog("LDAP: \$result: ".var_export($result, true));
+		return $result;
 	}
-	
+
 	/*
 	 * Make an LDAP-bind to authenticate
 	 * @param ldapServer
@@ -285,22 +188,22 @@ class ldapLogon {
 
 	/**
 	 * getLogon is used to return the userobj without authentication, used for cookie-stuf
-	 * 
+	 *
 	 * @param id of the user, should be -1 if using ldap-logon
 	 */
 	static function getLogon($id) {
 		global $_zp_current_admin_obj;
 		if ($id == -1 ){
-                        if (DEBUG_LOGIN) { debugLog("Using LDAP Authorization, getting LDAP-Cookie: User=".zp_getCookie('zp_user_auth_ldap')); }
-                        if($_zp_current_admin_obj = self::checkLogon(zp_getCookie('zp_user_auth_ldap'))) {
-                                return $_zp_current_admin_obj->getRights();
-                        }
-                }
+			if (DEBUG_LOGIN) { debugLog("Using LDAP Authorization, getting LDAP-Cookie: User=".zp_getCookie('zp_user_auth_ldap')); }
+			if($_zp_current_admin_obj = self::checkLogon(zp_getCookie('zp_user_auth_ldap'))) {
+				return $_zp_current_admin_obj->getRights();
+			}
+		}
 	}
-	
+
 	/**
 	 * checkLogon for LDAP
-	 * 
+	 *
 	 * @param user is the username
 	 * @param pass can be null if no authentication should be done
 	 */
@@ -308,74 +211,77 @@ class ldapLogon {
 		$auth = 'external';
 		$searchfor = array('`user`=' => $user,  '`valid`=' => 1);
 		$userobj = Zenphoto_Authority::getAnAdmin($searchfor);
-		if (!$userobj ) {
-			$result = ldapLogon::getExternalAuthArray(getOption('ldapServer'),getOption('ldapServerPort'),getOption('ldapDc'),$user,getOption('ldapZenDefaultTemplate'));
+		if (!$userobj) {
+			$result = ldapLogon::getExternalAuthArray(getOption('ldapServer'),getOption('ldapServerPort'),getOption('ldapSearchBase'),$user,getOption('ldapZenDefaultTemplate'));
 			if($result){
+				$user = $result['id'];
 				unset($result['id']);
 				unset($result['user']);
 				$authority = '';
+				//	create a transient user
 				$userobj = new Zenphoto_Administrator('', 1);
 				$userobj->setUser($user);
 				$userobj->setRights(NO_RIGHTS);
+					//	Flag as external credentials for completeness
 				$properties = array_keys($result);
 				array_unshift($properties, $auth);
 				$userobj->setCredentials($properties);
-				$member = false; 
+				$member = false;
 				foreach ($result as $key=>$value) {
 					switch ($key) {
-						case 'authority':
-							$authority = '::'.$value;
-							unset($result['authority']);
-							break;
-						case 'groups':
-							$rights = NO_RIGHTS;
-							$objects = array();
-							$groups = $value;
-							foreach ($groups as $key=>$group) {
-								if (DEBUG_LOGIN){ debugLog("LDAP: Adding Group: $group"); }
-								$groupobj = Zenphoto_Authority::getAnAdmin(array('`user`=' => $group,'`valid`=' => 0));
-								if ($groupobj) {
-									$member = true;
-									$rights = $groupobj->getRights() | $rights;
-									$objects = array_merge($groupobj->getObjects(), $objects);
-									if ($groupobj->getName() == 'template') {
-										unset($groups[$key]);
-									}
-								} else {
+					case 'authority':
+						$authority = '::'.$value;
+						unset($result['authority']);
+						break;
+					case 'groups':
+						$rights = NO_RIGHTS;
+						$objects = array();
+						$groups = $value;
+						foreach ($groups as $key=>$group) {
+							if (DEBUG_LOGIN){ debugLog("LDAP: Adding Group: $group"); }
+							$groupobj = Zenphoto_Authority::getAnAdmin(array('`user`=' => $group,'`valid`=' => 0));
+							if ($groupobj) {
+								$member = true;
+								$rights = $groupobj->getRights() | $rights;
+								$objects = array_merge($groupobj->getObjects(), $objects);
+								if ($groupobj->getName() == 'template') {
 									unset($groups[$key]);
 								}
+							} else {
+								unset($groups[$key]);
 							}
-							if ($member) {
-								$userobj->setGroup(implode(',',$groups));
+						}
+						if ($member) {
+							$userobj->setGroup(implode(',',$groups));
+							$userobj->setRights($rights);
+							$userobj->setObjects($objects);
+						}
+						break;
+					case 'defaultgroup':
+						if (!$member && isset($result['defaultgroup'])) {
+							$group = $result['defaultgroup'];
+							$groupobj = Zenphoto_Authority::getAnAdmin(array('`user`=' => $group,'`valid`=' => 0));
+							if ($groupobj) {
+								$rights = $groupobj->getRights();
+								$objects = $groupobj->getObjects();
+								if ($groupobj->getName() != 'template') {
+									$group = NULL;
+								}
+								$userobj->setGroup($group);
 								$userobj->setRights($rights);
 								$userobj->setObjects($objects);
 							}
-							break;
-						case 'defaultgroup':
-							if (!$member && isset($result['defaultgroup'])) {
-								$group = $result['defaultgroup'];
-								$groupobj = Zenphoto_Authority::getAnAdmin(array('`user`=' => $group,'`valid`=' => 0));
-								if ($groupobj) {
-									$rights = $groupobj->getRights();
-									$objects = $groupobj->getObjects();
-									if ($groupobj->getName() != 'template') {
-										$group = NULL;
-									}
-									$userobj->setGroup($group);
-									$userobj->setRights($rights);
-									$userobj->setObjects($objects);
-								}
-							}
-							break;
-						case 'objects':
-							$userobj->setObjects($objects);
-							break;
-						case 'album':
-							$userobj->createPrimealbum(false, $value);
-							break;
-						default:
-							$userobj->set($key,$value);
-							break;
+						}
+						break;
+					case 'objects':
+						$userobj->setObjects($objects);
+						break;
+					case 'album':
+						$userobj->createPrimealbum(false, $value);
+						break;
+					default:
+						$userobj->set($key,$value);
+						break;
 					}
 				}
 				$properties = array_keys($result);
@@ -386,17 +292,18 @@ class ldapLogon {
 			}
 		} else {
 			$userobj = NULL;	// User exists in local DB, it should be authenticated before, some error?
-		}	
+		}
 		if (isset($result['logout_link'])) {
 			$userobj->logout_link = $result['logout_link'];
 		}
 		if ($pass != NULL && $userobj) {
-			$ldapRdn = 'cn='.$userobj->get('user').','.getOption('ldapOu').','.getOption('ldapDc');
+			$ldapRdn = getOption("ldapAuthAttribute").'='.$user.','.getOption('ldapSearchBase');
 			if(!ldapLogon::authenticateLdapUser(getOption('ldapServer'), getOption('ldapServerPort'), $ldapRdn, $pass)) {
 				$userobj = NULL;
 			}
 		}
+		debugLog("LDAP: \$userobj: ".var_export($userobj, true));
 		return $userobj;
-	}	
+	}
 }
 ?>
