1ad704714f0c8b17b1362b071ee6a691384afee3
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2017-04-07 09:37:39 +0200

    more options

--- ldapLogon.php
+++ ldapLogon.php
@@ -18,13 +18,15 @@ zp_register_filter('','ldapLogon::getLogon');
 class ldapLogon {
 	function __construct(){
 		setOptionDefault('ldapEnabled', 1);
-		setOptionDefault('ldapType', 0);
 		setOptionDefault('ldapServer', '192.168.1.14');
 		setOptionDefault('ldapServerPort','389');
-		setOptionDefault('ldapDc','dc=DOMAIN');
-		setOptionDefault('ldapOu','ou=Users');
-		setOptionDefault('ldapRdrDn','cn=reader,dc=DOMAIN');
-		setOptionDefault('ldapRdrPass','test');
+		setOptionDefault('ldapSearchBase','cn=users,cn=accounts,dc=example,dc=com');
+		setOptionDefault('ldapSearchBaseGroup','cn=groups,cn=accounts,dc=example,dc=com');
+		setOptionDefault('ldapLoginAttribute','cn');
+		setOptionDefault('ldapAuthAttribute','cn');
+		setOptionDefault('ldapUserFilter','');
+		setOptionDefault('ldapReaderDn','cn=reader,dc=example,dc=com');
+		setOptionDefault('ldapReaderPass','test');
 		setOptionDefault('ldapZenDefaultTemplate','extern');
 		setOption('zp_plugin_ldapLogon','4096');
 	}
@@ -35,49 +37,56 @@ class ldapLogon {
 								'order' => 0,
 								'desc' => gettext("Set the checkbox to enable the LDAP logon functionality")
 							),
-			gettext('Type of directory') => array('key' => 'ldapType',
-                                                                'type' => OPTION_TYPE_RADIO,
-								'buttons' => array(getText('Microsoft Active Directory') => "ldapTypeAD",getText("OpenLDAP") => "ldapTypeOL"),
-                                                                'order' => 1,
-                                                                'desc' => gettext("Set LDAP type: Microsoft AD or OpenLDAP")
-                                                        ),
 			gettext('LDAP Server IP/Hostname') => array('key' => 'ldapServer',
 								'type' => OPTION_TYPE_TEXTBOX,
-								'order' => 2,
+								'order' => 1,
 								'desc' => gettext("Set the LDAP server IP")
 							),
 			gettext('LDAP Server Port') => array('key' => 'ldapServerPort',
 								'type' => OPTION_TYPE_TEXTBOX,
-								'order' => 3,
+								'order' => 2,
 								'desc' => gettext("Set the LDAP server port")
 							),
-			gettext('LDAP DC') => array('key' => 'ldapDc',
+			gettext('LDAP Search Base') => array('key' => 'ldapSearchBase',
+								'type' => OPTION_TYPE_TEXTBOX,
+								'order' => 3,
+								'desc' => gettext("Set the LDAP search base for users"),
+							),
+			gettext('LDAP Search Base for Groups') => array('key' => 'ldapSearchBaseGroup',
 								'type' => OPTION_TYPE_TEXTBOX,
 								'order' => 4,
-								'desc' => gettext("Set the LDAP DC e.g. dc=domain (--> the complete User-DN is dc=Test,ou=Users,dc=domain)")
+								'desc' => gettext("Set the LDAP search base for groups"),
 							),
-			gettext('LDAP OU') => array('key' => 'ldapOu',
+			gettext('LDAP Login Attribute') => array('key' => 'ldapLoginAttribute',
 								'type' => OPTION_TYPE_TEXTBOX,
 								'order' => 5,
-								'desc' => gettext("Set the LDAP OU e.g. ou=Users (--> the complete User-DN is dc=Test,ou=Users,dc=domain), for MS AD you have to use cn=Users!")
+								'desc' => gettext("Set the LDAP attribute the users must enter on ZenPhoto to login (e.g. mail to login with mailaddress, uid to login with username, etc.)"),
 							),
-			gettext('LDAP Reader DN') => array('key' => 'ldapRdrDn',
-                                                                'type' => OPTION_TYPE_TEXTBOX,
-                                                                'order' => 6,
-                                                                'desc' => gettext("Set the DN of an LDAP-user with read permissions for the memberOf group-attributes, e.g.: cn=reader,dc=loww, for MS AD the regular login can be used e.g. user@domain.tld")
-                                                        ),
-			gettext('LDAP Reader Password') => array('key' => 'ldapRdrPass',
-                                                                'type' => OPTION_TYPE_PASSWORD,
-                                                                'order' => 7,
-                                                                'desc' => gettext("Password of the LDAP-User with read permissions to acces the memberOf group-attribute")
-                                                        ),
-			gettext('Default template') => array('key' => 'ldapZenDefaultTemplate',
+			gettext('LDAP Auth Attribute') => array('key' => 'ldapAuthAttribute',
+								'type' => OPTION_TYPE_TEXTBOX,
+								'order' => 6,
+								'desc' => gettext("Set the LDAP attribute used to authenticate against LDAP (e.g. the first part of a fully distinguished name)"),
+							),
+			gettext('LDAP User Filter') => array('key' => 'ldapUserFilter',
+								'type' => OPTION_TYPE_TEXTBOX,
+								'order' => 7,
+								'desc' => gettext("Set an additional filter for users (e.g.: (memberOf=cn=zenphoto-user,cn=groups,cn=accounts,dc=example,dc=com) to only allow users that are members of the zenphoto-user LDAP group to log in)"),
+							),
+			gettext('LDAP Reader DN') => array('key' => 'ldapReaderDn',
 								'type' => OPTION_TYPE_TEXTBOX,
 								'order' => 8,
+								'desc' => gettext("Set the DN of an LDAP-user with read permissions for the user and group objects"),
+							),
+			gettext('LDAP Reader Password') => array('key' => 'ldapReaderPass',
+								'type' => OPTION_TYPE_PASSWORD,
+								'order' => 9,
+								'desc' => gettext("Password of the LDAP-User with read permissions for the user and group objects"),
+							),
+			gettext('Default template') => array('key' => 'ldapZenDefaultTemplate',
+								'type' => OPTION_TYPE_TEXTBOX,
+								'order' => 10,
 								'desc' => gettext("Template that should be used when no ldap zen-group is available")
 							)
-
-
 		);
 		return $options;
 	}
